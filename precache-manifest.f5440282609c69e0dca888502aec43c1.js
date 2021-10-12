38aaf9ba710abb9f21ae829c94ec05eac44ac39b
jbond <jbondagile@gmail.com>
2021-10-12 11:01:00 -0500

    Updates

---
+++ precache-manifest.f5440282609c69e0dca888502aec43c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2546198a3986f0f6cb7600bca2a911ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "023de00d0ebd2b08b27b",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "1491cd304625a29f1187",
+    "url": "/static/css/main.cea6551e.chunk.css"
+  },
+  {
+    "revision": "023de00d0ebd2b08b27b",
+    "url": "/static/js/2.69e7e361.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.69e7e361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1491cd304625a29f1187",
+    "url": "/static/js/main.09520f2c.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
